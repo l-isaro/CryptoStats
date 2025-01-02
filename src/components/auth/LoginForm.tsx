13c7ce4ad1512dd87e313a3979fbca0b804a8201
l-isaro <l.isaro@alustudent.com>
2025-01-02 17:26:42 +0200

    add dark mode support and enhance LoginForm with Material-UI components

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -1,5 +1,14 @@
+import { Button, TextField } from "@mui/material";
+
 export default function LoginForm() {
   return (
-    <div>loginForm</div>
+    <div className="flex flex-col gap-8 justify-center items-center h-screen">
+      <h1 className="text-6xl font-medium">Cryptostats</h1>
+      <div className="flex flex-col gap-4">
+        <TextField label="Email" className="w-80" type="email" required/>
+        <TextField label="Password" className="w-80" type="password" required/>
+      </div>
+      <Button variant="contained" className="w-80"><span className="p-1">Log In</span></Button>
+    </div>
   )
 }
